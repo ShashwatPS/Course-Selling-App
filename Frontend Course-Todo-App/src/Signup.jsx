d1f7ab588a567ec4a8d85c5bffb87184ab56b85e
Shashwat Singh <shashwat123student@gmail.com>
2023-08-08 01:28:52 +0530

    Designed the Sign in Page

--- Frontend Course-Todo-App/src/Signup.jsx
+++ Frontend Course-Todo-App/src/Signup.jsx
@@ -1,59 +1,125 @@
 import Button from '@mui/material/Button';
-import {Card, TextField, Typography} from "@mui/material";
+import {
+    Avatar,
+    Box,
+    Container, createTheme,
+    CssBaseline,
+    Grid,
+    Link,
+    TextField, ThemeProvider,
+    Typography
+} from "@mui/material";
 import {useState} from "react";
+
+function Copyright(props) {
+    return (
+        <Typography variant="body2" color="text.secondary" align="center" {...props}>
+            {'Copyright © '}
+            <Link color="inherit" href="https://mui.com/">
+                Your Website
+            </Link>{' '}
+            {new Date().getFullYear()}
+            {'.'}
+        </Typography>
+    );
+}
+
+const defaultTheme = createTheme();
+
 function Signup(){
     const [email,setEmail] =useState("");
     const [password,setPassword]=useState("");
     return(
         <div>
-        <div style={{
-            marginTop: 150,
-            marginBottom: 10,
-            textAlign: "center"
-        }}>
-            <Typography variant="h6">
-            Welcome to Coursera. Sign up Below
-            </Typography>
-         </div>
-            <center>
-                <Card variant="outlined" style={{width: 400,padding: 20}}>
-                <TextField
-                    onChange={(e)=>{
-                        setEmail(e.target.value);
-                    }}
-                    id="outlined-basic" label="Username" variant="outlined" fullWidth={true}/>
-            <br/><br/>
-                <TextField
-                    onChange={(e)=>{
-                        setPassword(e.target.value);
-                    }}
-                    id="outlined-basic" label="Password" variant="outlined" fullWidth={true}/>
-            <br/><br/>
-                <Button
-                    onClick={()=>{
-                        function callback2(data){
-                            localStorage.setItem("token",data.token);
-                            window.location= "/"
-                        }
+            <ThemeProvider theme={defaultTheme}>
+                <Container component="main" maxWidth="xs">
+                    <CssBaseline />
+                    <Box
+                        sx={{
+                            marginTop: 8,
+                            display: 'flex',
+                            flexDirection: 'column',
+                            alignItems: 'center',
+                        }}
+                    >
+                        <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+                            {/*<LockOutlinedIcon />*/}
+                        </Avatar>
+                        <Typography component="h1" variant="h5">
+                            Sign in
+                        </Typography>
+                        <Box component="form" noValidate sx={{ mt: 1 }}>
+                            <TextField
+                                margin="normal"
+                                required
+                                fullWidth
+                                id="email"
+                                label="Email Address"
+                                name="email"
+                                autoComplete="email"
+                                autoFocus
+                                onChange={(e)=>{
+                                    setEmail(e.target.value);}}
+                            />
+                            <TextField
+                                margin="normal"
+                                required
+                                fullWidth
+                                name="password"
+                                label="Password"
+                                type="password"
+                                id="password"
+                                autoComplete="current-password"
+                               onChange={(e)=>{
+                                   setPassword(e.target.value);
+                               }}
+                            />
+                            <Button
+                                type="submit"
+                                fullWidth
+                                variant="contained"
+                                sx={{ mt: 3, mb: 2 }}
+                                onClick={()=>{
+                                    function callback2(data){
+                                        localStorage.setItem("token",data.token);
+                                        window.location= "/"
+                                    }
 
-                        function callback1(res){
-                            res.json().then(callback2);
-                        }
+                                    function callback1(res){
+                                        res.json().then(callback2);
+                                    }
 
-                        fetch("http://localhost:3000/admin/signup",{
-                            method: "POST",
-                            body: JSON.stringify({
-                                username: email,
-                                password
-                            }),
-                            headers: {
-                                "Content-type": "application/json"
-                            }
-                        }).then(callback1)
-                    }}
-                    variant="contained">Sign up</Button>
-                </Card>
-            </center>
+                                    fetch("http://localhost:3000/admin/signup",{
+                                        method: "POST",
+                                        body: JSON.stringify({
+                                            username: email,
+                                            password
+                                        }),
+                                        headers: {
+                                            "Content-type": "application/json"
+                                        }
+                                    }).then(callback1)
+                                }}
+                            >
+                                Sign In
+                            </Button>
+                            <Grid container>
+                                <Grid item xs>
+                                    <Link href="#" variant="body2">
+                                        Forgot password?
+                                    </Link>
+                                </Grid>
+                                <Grid item>
+                                    <Link href="#" variant="body2">
+                                        {"Already have a account ? SignIn"}
+                                    </Link>
+                                </Grid>
+                            </Grid>
+                        </Box>
+                    </Box>
+                    <Copyright sx={{ mt: 8, mb: 4 }} />
+                </Container>
+            </ThemeProvider>
     </div>
     )
 }
