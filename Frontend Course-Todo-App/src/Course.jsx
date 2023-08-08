e7c4de27f749fdcf82e584eba76ce6433adb58b0
Shashwat Singh <shashwat123student@gmail.com>
2023-08-08 22:25:43 +0530

    Fixing the updating Course Page

--- Frontend Course-Todo-App/src/Course.jsx
+++ Frontend Course-Todo-App/src/Course.jsx
@@ -1,7 +1,33 @@
 import {useEffect, useState} from "react";
 import {useParams} from "react-router-dom";
-import {Card, TextField, Typography} from "@mui/material";
+import {
+    Avatar,
+    Box,
+    Card,
+    CardActionArea,
+    CardActions,
+    CardContent,
+    CardMedia,
+    Container, createTheme, CssBaseline, Grid, Link,
+    TextField, ThemeProvider,
+    Typography
+} from "@mui/material";
 import Button from "@mui/material/Button";
+
+function Copyright(props) {
+    return (
+        <Typography variant="body2" color="text.secondary" align="center" {...props}>
+            {'Copyright © '}
+            <Link color="inherit" href="https://mui.com/">
+                Your Website
+            </Link>{' '}
+            {new Date().getFullYear()}
+            {'.'}
+        </Typography>
+    );
+}
+
+const defaultTheme = createTheme();
 
 function Course(){
     let {courseId} = useParams();
@@ -40,7 +66,10 @@ function Course(){
     }
 
     return(
-        <div>
+        <div style={{
+            display: "flex",
+            justifyContent: "space-evenly"
+        }}>
             <CourseTable course={course}></CourseTable>
             <UpdateCard courses={courses} course={course} setCourses={setCourses}></UpdateCard>
         </div>
@@ -55,79 +84,137 @@ function UpdateCard(props){
 
     return(
         <div>
-            <center>
-                <Card variant="outlined" style={{width: 400,padding: 20}}>
-                    <TextField
-                        onChange={(e)=>{
-                            setTitle(e.target.value);
+            <ThemeProvider theme={defaultTheme}>
+                <Container component="main" maxWidth="xs">
+                    <CssBaseline />
+                    <Box
+                        sx={{
+                            marginTop: 8,
+                            display: 'flex',
+                            flexDirection: 'column',
+                            alignItems: 'center',
                         }}
-                        id="outlined-basic" label="Title" variant="outlined" fullWidth={true}/>
-                    <TextField
-                        onChange={(e)=>{
-                            setdescription(e.target.value);
-                        }}
-                        id="outlined-basic" label="Description" variant="outlined" fullWidth={true}/>
-                    <TextField
-                        onChange={(e)=>{
-                            setImage(e.target.value);
-                        }}
-                        id="outlined-basic" label="Image Link" variant="outlined" fullWidth={true}/>
-                    <Button
-                        onClick={()=>{
-                            function callback2(data){
-                                alert("Course Updated!");
-                                let updatedCourses = [];
-                                for(let i=0;i<props.courses.length;i++){
-                                    if (props.courses[i].id == course.id){
-                                        updatedCourses.push({
-                                            id: course.id,
+                    >
+                        <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}
+                                src={"https://media.licdn.com/dms/image/D4D03AQEV_117eQkZLQ/profile-displayphoto-shrink_800_800/0/1686557768279?e=2147483647&v=beta&t=1huYlCYn91aB2b19p9kcT1DkudHGIvk7cHYo6FErxBg"}>
+                        </Avatar>
+                        <Typography component="h1" variant="h5">
+                            Update Course
+                        </Typography>
+                        <Box component="form" sx={{ mt: 3 }}>
+                            <Grid container spacing={2}>
+                                <Grid item xs={12}>
+                                    <TextField
+                                        required
+                                        fullWidth
+                                        label="Title"
+                                        name="Name"
+                                        onChange={(e)=>{
+                                            setTitle(e.target.value);
+                                        }}
+                                    />
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <TextField
+                                        required
+                                        fullWidth
+                                        label="Description"
+                                        name="Description"
+                                        onChange={(e)=>{
+                                            setdescription(e.target.value);
+                                        }}
+                                    />
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <TextField
+                                        required
+                                        fullWidth
+                                        name="imageLink"
+                                        label="Image Link"
+                                        onChange={(e)=>{
+                                            setImage(e.target.value);
+                                        }}
+                                    />
+                                </Grid>
+                            </Grid>
+                            <Button
+                                type="submit"
+                                fullWidth
+                                variant="contained"
+                                sx={{ mt: 3, mb: 2 }}
+                                onClick={()=>{
+                                    function callback2(data){
+                                        alert("Course Updated!");
+                                        let updatedCourses = [];
+                                        for(let i=0;i<props.courses.length;i++){
+                                            if (props.courses[i].id == course.id){
+                                                updatedCourses.push({
+                                                    id: course.id,
+                                                    title: title,
+                                                    description: description,
+                                                    imageLink: image
+                                                })
+                                            }
+                                            else{
+                                                updatedCourses.push(props.courses[i]);
+                                            }
+                                            props.setCourses(updatedCourses);
+                                        }}
+
+                                    function callback1(res){
+                                        res.json().then(callback2);
+                                    }
+
+                                    fetch("http://localhost:3000/admin/courses/"+ course.id,{
+                                        method: "PUT",
+                                        body: JSON.stringify({
                                             title: title,
                                             description: description,
-                                            imageLink: image
-                                        })
-                                    }
-                                    else{
-                                        updatedCourses.push(props.courses[i]);
-                                }
-                                    props.setCourses(updatedCourses);
-                            }}
-
-                            function callback1(res){
-                                res.json().then(callback2);
-                            }
-
-                            fetch("http://localhost:3000/admin/courses/"+ course.id,{
-                                method: "PUT",
-                                body: JSON.stringify({
-                                    title: title,
-                                    description: description,
-                                    imageLink: image,
-                                    published: true
-                                }),
-                                headers: {
-                                    "Content-type": "application/json",
-                                    "Authorization": "Bearer "+ localStorage.getItem("token"),
-                                }
-                            }).then(callback1)
-                        }}
-                        variant="contained">Update Course</Button>
-                </Card>
-            </center>
+                                            imageLink: image,
+                                            published: true
+                                        }),
+                                        headers: {
+                                            "Content-type": "application/json",
+                                            "Authorization": "Bearer "+ localStorage.getItem("token"),
+                                        }
+                                    }).then(callback1)}}
+                            >
+                                Add Course
+                            </Button>
+                        </Box>
+                    </Box>
+                    <Copyright sx={{ mt: 5 }} />
+                </Container>
+            </ThemeProvider>
         </div>
     )
 }
 function CourseTable(props){
     const course = props.course;
     return(
-            <Card style={{
-                margin: 10,
-                width: 300,
-                minHeight: 200
-            }}>
-                <Typography textAlign={"center"} variant={"h5"}>{course.title}</Typography>
-                <Typography textAlign={"center"} variant={"subtitle1"}>{course.description}</Typography>
-                <img src={course.imageLink} style={{width:300}} alt={"ImageLink"}></img>
-            </Card>
+        <Card style={{
+            margin: 25,
+            width: 300,
+            minHeight: 200,
+            marginTop: 45
+        }}>
+            <CardActionArea>
+                <CardMedia
+                    component="img"
+                    height="200"
+                    image={course.imageLink}
+                    alt="Course Image"
+                />
+                <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                        {course.title}
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                        {course.description}
+                    </Typography>
+                </CardContent>
+            </CardActionArea>
+        </Card>
     )
 }
 export default Course;
