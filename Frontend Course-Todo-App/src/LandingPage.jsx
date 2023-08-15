39f7ec55764b3430b15e538fec143f39865ec9fb
Shashwat Singh <shashwat123student@gmail.com>
2023-08-15 22:47:02 +0530

    Making the Landing Page Prettier

--- Frontend Course-Todo-App/src/LandingPage.jsx
+++ Frontend Course-Todo-App/src/LandingPage.jsx
@@ -1,30 +1,40 @@
-import {Typography} from "@mui/material";
 import './App.css';
+import {Grid} from "@mui/material";
+import "./LandingPageCSS.css"
 function LandingPage(){
     return(
         <div>
-            <div className={"welcome"}
+            <Grid container>
+                <Grid item lg={7}>
+            <div
                 style={{
                 height: "30vh",
                 textAlign: "center",
                 paddingTop: 170
             }}>
+                <div className={"title"}>
                 WELCOME TO COURSERA
+                </div>
             </div>
-            <div className={"wtext"} style={{
+            <div style={{
                 marginLeft: 190,
                 marginRight:190,
                 textAlign: "center",
                 marginTop: 75,
             }}>
+                <div className={"text"}>
                 ❝ Welcome to Coursera Embark on a journey of knowledge and growth with our carefully crafted courses.
                 Whether you're looking to enhance your skills, explore new subjects, or embrace lifelong learning, Coursera offers a
-                diverse selection of courses to suit your interests. Explore our comprehensive range of courses designed to empower you on
-                your educational path. From cutting-edge technology to timeless classics, we've curated a collection that caters to learners
-                of all backgrounds and aspirations. Join our vibrant community of learners, where curiosity knows no bounds and learning knows
-                no limits. As you navigate through our platform, you'll discover a seamless experience that's user-friendly and inspiring.
-                Thank you for choosing Coursera for your learning journey. Here's to unlocking new horizons together ❞
+                diverse selection of courses to suit your interests.
+                </div>
             </div>
+                </Grid>
+                <Grid item lg={5}>
+                    <div className={"Image"} justifyContent="center" alignItems="center">
+                    <img src={"https://wallpapercave.com/wp/wp6596813.jpg"} alt={"Teaching Image"} />
+                    </div>
+                </Grid>
+            </Grid>
         </div>
     )
 }
